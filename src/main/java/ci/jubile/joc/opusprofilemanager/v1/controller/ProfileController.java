165a66e5bdfeaa2f12060776d7d380424a3d654c
koffiange <koffiange62@gmail.com>
2023-03-07 18:36:45 -0500

    Change Log : + Ajout de ResponseEntity dans les peponse des endpoint;

--- src/main/java/ci/jubile/joc/opusprofilemanager/v1/controller/ProfileController.java
+++ src/main/java/ci/jubile/joc/opusprofilemanager/v1/controller/ProfileController.java
@@ -7,10 +7,12 @@ import ci.jubile.joc.opusprofilemanager.v1.mapper.ProfileMapper;
 import ci.jubile.joc.opusprofilemanager.v1.resource.ProfileResource;
 import ci.jubile.joc.opusprofilemanager.v1.service.PasswordService;
 import ci.jubile.joc.opusprofilemanager.v1.service.ProfileServiceImpl;
+import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -30,38 +32,42 @@ public class ProfileController {
     }
 
     @GetMapping
-    public List<ProfileResource> findAll(){
-        return profileServiceImpl.findAll().stream().map(profileMapper::profileToProfileResource).collect(Collectors.toList());
+    public ResponseEntity<List<ProfileResource>> findAll(){
+        List<ProfileResource> profileResourceList = profileServiceImpl.findAll().stream()
+                .map(profileMapper::profileToProfileResource).collect(Collectors.toList());
+        return ResponseEntity.ok(profileResourceList);
     }
 
     @PostMapping("profile")
-    public ProfileResource create(@Valid @RequestBody ProfileResource profileResource){
+    public ResponseEntity<ProfileResource> create(@Valid @RequestBody ProfileResource profileResource){
         Profile profile = profileMapper.profileResourceToProfile(profileResource);
 
         profile = profileServiceImpl.create(profile);
         passwordService.create(profile.getId(), profileResource.getPassword());
 
-        return profileMapper.profileToProfileResource(profile);
+        String uri = "/profile/"+profile.getId();
+        return ResponseEntity.created(URI.create(uri)).body(profileMapper.profileToProfileResource(profile)) ;
     }
 
     @GetMapping("profile/{id}")
-    public ProfileResource findById(@PathVariable(name = "id") String id) throws ProfileNotFoundException {
+    public ResponseEntity<ProfileResource> findById(@PathVariable(name = "id") String id) throws ProfileNotFoundException {
         Profile profile = profileServiceImpl.findById(id);
-        return profileMapper.profileToProfileResource(profile);
+        return ResponseEntity.ok(profileMapper.profileToProfileResource(profile));
     }
 
     @PutMapping("profile")
-    public ProfileResource updateProfile(@Valid @RequestBody ProfileResource profileResource){
+    public ResponseEntity<ProfileResource> updateProfile(@Valid @RequestBody ProfileResource profileResource){
         Profile profileToUpdate = profileMapper.profileResourceToProfile(profileResource);
         Profile updated = profileServiceImpl.update(profileToUpdate);
-        return profileMapper.profileToProfileResource(updated);
+        return ResponseEntity.ok(profileMapper.profileToProfileResource(updated));
     }
 
     @PutMapping("profile/enabling-or-desabling/{id}")
-    public void changeProfileEnablingStatus(@PathVariable(name = "id") String id,
+    public ResponseEntity<?> changeProfileEnablingStatus(@PathVariable(name = "id") String id,
                                             @RequestParam(name = "status") ProfileStatus status)
                                             throws ProfileNotFoundException {
         profileServiceImpl.enableOrDisableProfile(id, status);
+        return ResponseEntity.noContent().build();
     }
 
 }
