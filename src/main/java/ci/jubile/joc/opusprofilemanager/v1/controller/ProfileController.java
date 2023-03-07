f45073267b5e59abddeb48328c2e9e73b69eee09
koffiange <koffiange62@gmail.com>
2023-03-07 15:01:44 -0500

    Change Log : + Deplace l'utilisation des mappers dans le controller et non dans le service. Les service retournes des objet du model et les controller retournent des ressources.

--- src/main/java/ci/jubile/joc/opusprofilemanager/v1/controller/ProfileController.java
+++ src/main/java/ci/jubile/joc/opusprofilemanager/v1/controller/ProfileController.java
@@ -1,14 +1,19 @@
 package ci.jubile.joc.opusprofilemanager.v1.controller;
 
+import ci.jubile.joc.opusprofilemanager.domain.Profile;
 import ci.jubile.joc.opusprofilemanager.v1.enumeration.ProfileStatus;
 import ci.jubile.joc.opusprofilemanager.v1.exception.ProfileNotFoundException;
+import ci.jubile.joc.opusprofilemanager.v1.mapper.ProfileMapper;
+import ci.jubile.joc.opusprofilemanager.v1.resource.PasswordResource;
 import ci.jubile.joc.opusprofilemanager.v1.resource.ProfileResource;
 import ci.jubile.joc.opusprofilemanager.v1.service.ProfileServiceImpl;
+import org.springframework.util.Assert;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("v1/profiles")
@@ -16,29 +21,46 @@ import java.util.List;
 public class ProfileController {
 
     private final ProfileServiceImpl profileServiceImpl;
+    private final ProfileMapper profileMapper;
 
-    public ProfileController(ProfileServiceImpl profileServiceImpl) {
+    public ProfileController(ProfileServiceImpl profileServiceImpl, ProfileMapper profileMapper) {
         this.profileServiceImpl = profileServiceImpl;
+        this.profileMapper = profileMapper;
     }
 
     @GetMapping
     public List<ProfileResource> findAll(){
-        return profileServiceImpl.findAll();
+        return profileServiceImpl.findAll().stream().map(profileMapper::profileToProfileResource).collect(Collectors.toList());
     }
 
     @PostMapping("profile")
     public ProfileResource create(@Valid @RequestBody ProfileResource profileResource){
-        return profileServiceImpl.create(profileResource);
+        Profile profile = profileMapper.profileResourceToProfile(profileResource);
+        profile = profileServiceImpl.create(profile);
+
+        /*
+        PasswordResource passwordResource = PasswordResource.builder()
+                .profileId(profile.getId())
+                .newPassword(profileResource.getPassword())
+                .build();
+        Assert.notNull(passwordResource, "Password Resource est null");
+        passwordService.create(passwordResource);
+         */
+
+        return profileMapper.profileToProfileResource(profile);
     }
 
     @GetMapping("profile/{id}")
     public ProfileResource findById(@PathVariable(name = "id") String id) throws ProfileNotFoundException {
-        return profileServiceImpl.findById(id);
+        Profile profile = profileServiceImpl.findById(id);
+        return profileMapper.profileToProfileResource(profile);
     }
 
     @PutMapping("profile")
     public ProfileResource updateProfile(@Valid @RequestBody ProfileResource profileResource){
-        return profileServiceImpl.update(profileResource);
+        Profile profileToUpdate = profileMapper.profileResourceToProfile(profileResource);
+        Profile updated = profileServiceImpl.update(profileToUpdate);
+        return profileMapper.profileToProfileResource(updated);
     }
 
     @PutMapping("profile/enabling-or-desabling/{id}")
