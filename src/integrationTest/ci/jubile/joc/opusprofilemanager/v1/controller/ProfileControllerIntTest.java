4ea336e208ff9aab88d4b85665b8220d7f93cf98
koffiange <koffiange62@gmail.com>
2023-03-30 02:02:56 -0400

    Change Log : + Creation de la task gradle "integrationTest", pour la gestion des tests d'intégration.

--- src/integrationTest/ci/jubile/joc/opusprofilemanager/v1/controller/ProfileControllerIntTest.java
+++ src/integrationTest/ci/jubile/joc/opusprofilemanager/v1/controller/ProfileControllerIntTest.java
@@ -3,20 +3,20 @@ package ci.jubile.joc.opusprofilemanager.v1.controller;
 import ci.jubile.joc.opusprofilemanager.v1.resource.ProfileResource;
 import ci.jubile.joc.opusprofilemanager.v1.utils.ProfileTestUtils;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.util.Assert;
 import org.springframework.web.context.WebApplicationContext;
 
 import java.net.URI;
 
+import static org.hamcrest.Matchers.hasSize;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -24,6 +24,10 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @SpringBootTest
 @WebAppConfiguration
 public class ProfileControllerIntTest {
+
+    private static String BASE_URI = "http://localhost:8080/v1/profiles";
+
+    private static String EMAIL = "koffiange@siracide.com";
 
     private static ObjectMapper objectMapper;
 
@@ -43,17 +47,26 @@ public class ProfileControllerIntTest {
     }
 
     @Test
-    @DisplayName(value = "Profile create() methode integration test.")
+    @DisplayName(value = "Profile create() methode int. test.")
     void create() throws Exception {
-        String BASE_URI = "http://localhost:8080/v1/profiles";
         URI uri = URI.create(BASE_URI + "/profile");
         ProfileResource profileResource = ProfileTestUtils.profileResourceBuilder();
-        String EMAIL = "koffiange@siracide.com";
         mockMvc.perform(post(uri)
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(objectMapper.writeValueAsBytes(profileResource)))
                 .andExpect(status().isCreated())
                 .andExpect(jsonPath("$").exists())
                 .andExpect(jsonPath("$.email").value(EMAIL));
+    }
+
+    @Test
+    @DisplayName(value = "Profile findAll() methode int. Test.")
+    void findAll() throws Exception {
+        URI uri = URI.create(BASE_URI);
+        mockMvc.perform(get(uri)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$").exists())
+                .andExpect(jsonPath("$[0].email").value(EMAIL));
     }
 }
